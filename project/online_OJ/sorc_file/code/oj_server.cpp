7c824760ab02412d8ad2e82b8b506cbab4272b86
Yfan133 <test@localhost.localdomain>
2021-01-18 20:16:11 +0800

    online_Oj

--- project/online_OJ/sorc_file/code/oj_server.cpp
+++ project/online_OJ/sorc_file/code/oj_server.cpp
@@ -2,12 +2,21 @@
 #include <cstdio>
 
 #include "httplib.h"
+#include "oj_model.hpp"
 int main()
 {
   using namespace httplib;
   Server svr;
+  OjModel model;
+
   // 1.注册所有题目的资源路径
-  svr.Get("/all_questions", [](const Request &req, Response &resp){
+  svr.Get("/all_questions", [&model](const Request &req, Response &resp){
+      std::vector<Questions> all_ques;
+      model.GetAllQuestions(&all_ques);
+      for (auto ques : all_ques)
+      {
+        std::cout << ques.id_ << " " << ques.title_ << " " << ques.comp_ << " " << ques.path_ << std::endl;
+      }
       resp.set_content("<html><h1>this is Linux~</h1></html>", 36, "text/html");
       });
   // 2.注册单个题目的资源路径 
--- project/online_OJ/sorc_file/code/tools.hpp
+++ project/online_OJ/sorc_file/code/tools.hpp
@@ -1,21 +1,38 @@
 #pragma once
 
 #include <iostream>
-
+#include <fstream>
+#include <string>
+#include <vector>
 #include <boost/algorithm/string.hpp>
 
 class FileOpen
 {
   public:
-    static void file_open(const std::string file_path, std::string* buf)
+    static bool file_open(const std::string file_path, std::string* content)
     {
-      buf.clear();
-      std::ifstream file();
-
+      (*content).clear();
+      std::ifstream file(file_path.c_str());
+      if (!file.is_open())
+      {
+        perror("open_file");
+        return false;
+      }
+      std::string buf;
+      while (std::getline(file, buf))
+      {
+        (*content) += buf + "\n";
+      }
+      file.close();
+      return true;
     }
 };
 
 class StringSpit
 {
   public:
+    static void Split(const std::string& input, const std::string& split_char, std::vector<std::string>* output)
+    {
+      boost::split(*output, input, boost::is_any_of(split_char), boost::token_compress_off);
+    }
 };
